c5e9fb64c59d6f0a6172837e95d0473b0ff4657e
Ubuntu <ubuntu@ip-172-31-42-87.eu-west-3.compute.internal>
2023-02-20 09:09:45 +0000

    m

--- rl_custom_games/custom_tetris/bin/hp_tt.py
+++ rl_custom_games/custom_tetris/bin/hp_tt.py
@@ -23,12 +23,11 @@ import torch as th
 
 @click.command()
 @click.option("--from_scratch", default=False, type=bool)
-@click.option("--total_timestep", default=300_000, type=int, show_default=True)
 @click.option("--board_height", default=8, type=int, show_default=True)
 @click.option("--board_width", default=4, type=int, show_default=True)
-@click.option("--brick_set", default="basic_ext", type=str, show_default=True)
-@click.option("--max_step", default=2000, type=int, show_default=True)
-def train_ttris(from_scratch, total_timestep, brick_set,
+@click.option("--brick_set", default="basic_ext2", type=str, show_default=True)
+@click.option("--max_step", default=30, type=int, show_default=True)
+def train_ttris(from_scratch, brick_set,
                 board_height,
                 board_width,
                 max_step):
@@ -39,22 +38,24 @@ def train_ttris(from_scratch, total_timestep, brick_set,
 
     def objective(trial: Trial):
         rand = trial.suggest_categorical("rand", [42,314])
-        n_envs = trial.suggest_categorical("n_envs", [8])
+        n_envs = trial.suggest_categorical("n_envs", [8,16])
         # Categorical parameter
-        learning_rate = 0.001  # trial.suggest_float("learning_rate", 0.0001, 0.001)
+        learning_rate =trial.suggest_categorical("learning_rate", [0.001]) # 0.001  # trial.suggest_float("learning_rate", 0.0001, 0.001)
 
-        batch_size = trial.suggest_categorical("batch_size", [32,64])
-        n_steps: int = trial.suggest_categorical("n_steps", [1024])
-        n_epochs = trial.suggest_categorical("n_epochs", [8,32])
-        # trial.suggest_categorical("batch_size", [64])
+        batch_size = trial.suggest_categorical("batch_size", [32])
+        n_steps: int = trial.suggest_categorical("n_steps", [4096,2048])
+        n_epochs = trial.suggest_categorical("n_epochs", [4,8,12])
+        clip_range = trial.suggest_categorical("clip_range", [ 0.3, 0.5,0.7])
+        vf_net_size = trial.suggest_categorical("vf_net_size", [ [96],[72],[64],[16,16],[32,32]  ])
+        pi_net_size = trial.suggest_categorical("pi_net_size", [ [96],[72],[64],[16,16],[32,32]  ])
 
-        vf_net_size = trial.suggest_categorical("vf_net_size", [4,32])
-        pi_net_size = trial.suggest_categorical("pi_net_size", [4,32])
+        gamma = trial.suggest_categorical("gamma", [ 0.9, 0.95,0.99,0.999])
+        total_timestep = trial.suggest_categorical("total_timestep", [3_000_000])
 
         #
         mlflow_client = mlflow.MlflowClient()
 
-        with ActiveRun(mlflow_client.create_run("0")) as active_run:
+        with ActiveRun(mlflow_client.create_run("4")) as active_run:
             run_id = active_run.info.run_id
 
             def log_param(k,v):
@@ -64,22 +65,26 @@ def train_ttris(from_scratch, total_timestep, brick_set,
             log_param("brick_set", brick_set)
             log_param("board_height", board_height)
             log_param("board_width", board_width)
-
+            log_param("max_step", max_step)
+            
             log_param("n_envs", n_envs)
             log_param("learning_rate", learning_rate)
 
             log_param("batch_size", batch_size)
             log_param("n_steps", n_steps)
             log_param("n_epochs", n_epochs)
+            
+            log_param("clip_range", clip_range)
 
             log_param("vf_net_size", vf_net_size)
             log_param("pi_net_size", pi_net_size)
+            log_param("gamma", gamma)
 
-            save_path = active_run.info.artifact_uri.replace("file:///E:/pychpj/testrl/", "")
+            save_path = active_run.info.artifact_uri.replace("s3://minio/yourfolder/", "mlruns/")
             log_param("save_path", save_path)
             log_param("rand", rand)
 
-
+            os.makedirs(os.path.join(save_path))
             mlflow_output = MLflowOutputFormat(mlflow_client,run_id)
 
             loggers = Logger(
@@ -128,7 +133,7 @@ def train_ttris(from_scratch, total_timestep, brick_set,
 
                 policy_kwargs = dict(
                     features_extractor_kwargs={})
-                policy_kwargs["net_arch"] = dict(pi=[pi_net_size], vf=[vf_net_size])
+                policy_kwargs["net_arch"] = dict(pi=pi_net_size, vf=vf_net_size)
                 policy_kwargs["normalize_images"] = False
 
                 policy_kwargs["optimizer_class"] = RMSpropTFLike
@@ -138,11 +143,12 @@ def train_ttris(from_scratch, total_timestep, brick_set,
                 model = PPO(ppo.MlpPolicy, env, device="cpu",
                            verbose=0,
                            learning_rate=pow_schedule(learning_rate,
-                                                        learning_rate/2,
+                                                        learning_rate/10,
                                                         gamma=2),
                            ent_coef=0.01,
                            vf_coef=0.5,
-                           clip_range=0.1,
+                           gamma=gamma,
+                           clip_range=clip_range,
                            batch_size=batch_size,
                            n_steps=n_steps,
                            n_epochs=n_epochs,
@@ -183,7 +189,7 @@ def train_ttris(from_scratch, total_timestep, brick_set,
         return 1
 
     study = optuna.create_study(sampler=optuna.samplers.BruteForceSampler())
-    study.optimize(objective, n_trials=32,n_jobs=4)
+    study.optimize(objective, n_trials=1,n_jobs=1)
 
 if __name__ == "__main__":
     train_ttris()
